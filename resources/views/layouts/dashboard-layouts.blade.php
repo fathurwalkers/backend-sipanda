b01d97d702a48c3551fea3abd5cb524e5886ba33
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-15 09:30:27 -0700

    update

--- resources/views/layouts/dashboard-layouts.blade.php
+++ resources/views/layouts/dashboard-layouts.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Toko Ayzam - Sistem Informasi Penjualan</title>
+    <title>SIPANDA - Sistem Informasi Pemilihan Nasional dan Daerah</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
